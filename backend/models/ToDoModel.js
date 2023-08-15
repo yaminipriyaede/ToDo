282051d4af380630056b49d2b48332c9e73799c8
yaminipriyaede <131669349+yaminipriyaede@users.noreply.github.com>
2023-08-15 12:57:09 +0530

    Update ToDoModel.js

--- backend/models/ToDoModel.js
+++ backend/models/ToDoModel.js
@@ -1,1 +1,11 @@
 
+const mongoose = require('mongoose');
+
+const todoSchema = mongoose.Schema({
+    text: {
+        type: String,
+        require: true
+    }
+});
+
+module.exports = mongoose.model('ToDo', todoSchema);
