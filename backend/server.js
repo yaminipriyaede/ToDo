a5306f6f77cfb33c24181b7113d73f157860c25d
yaminipriyaede <131669349+yaminipriyaede@users.noreply.github.com>
2023-08-15 12:49:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const route = require('./routes/ToDoRoute');
 
+const app = express();
+const port = 5500;
+
+app.use(express.json());
+
+
+mongoose.connect('mongodb+srv://yamini:yamini@cluster0.typrnx7.mongodb.net/?retryWrites=true&w=majority').then(() => {
+    console.log('Database connected !!!');
+}).catch(err => console.log(err.message));
+
+
+app.use(route);
+
+app.listen(port, () => console.log(`running on port : ${port}`));
